5b4e72cfca8db371394f616a35da76771bbd9cce
RyanL <ryanl@Labeebs-MacBook-Air-51.local>
2026-04-14 09:02:25 -0400

    Add main game loop in hgm.sh

--- hgm.sh
+++ hgm.sh
@@ -2,4 +2,14 @@
 
 echo "*--- HANGMAN ---*"
 read -s -p "Type Mystery Word" mystery
-echo " $mystery"
+echo 
+
+guesses=5
+
+while (( guesses > 0 )); do
+    echo "Guesses: $guesses"
+    read -n 1 -p "Type guess: " guess
+    echo
+
+    ((guesses--))
+done
