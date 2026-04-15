6381b9086377ffb6f39e7c297f4350790a61dcb8
RyanL <ryanl@Labeebs-MacBook-Air-51.local>
2026-04-15 11:53:30 -0400

    Add verification if guess was in mystery word

--- hgm.sh
+++ hgm.sh
@@ -5,11 +5,22 @@ read -s -p "Type Mystery Word" mystery
 echo 
 
 guesses=5
+guessed=""
 
 while (( guesses > 0 )); do
     echo "Guesses: $guesses"
     read -n 1 -p "Type guess: " guess
     echo
 
-    ((guesses--))
+    if [[ $mystery == *"$guess"* ]]; then
+        if [[ $guessed != *"$guess"* ]]; then
+            guessed+=$guess
+        fi
+
+        echo "$guess was in the word!"
+
+    else
+        ((guesses--))
+        echo "$guess was NOT in the word!"
+    fi
 done
