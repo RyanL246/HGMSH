7cabf76a9e05b1336a77628f1045f7f39898553a
RyanL <ryanl@Labeebs-MacBook-Air-51.local>
2026-04-15 12:17:24 -0400

    Add masked mystery word

--- hgm.sh
+++ hgm.sh
@@ -8,19 +8,34 @@ guesses=5
 guessed=""
 
 while (( guesses > 0 )); do
+    echo "-----------------"
     echo "Guesses: $guesses"
     read -n 1 -p "Type guess: " guess
-    echo
 
     if [[ $mystery == *"$guess"* ]]; then
         if [[ $guessed != *"$guess"* ]]; then
             guessed+=$guess
         fi
 
-        echo "$guess was in the word!"
+        echo " - \"$guess\" was in the word!"
 
     else
         ((guesses--))
-        echo "$guess was NOT in the word!"
+        echo " - \"$guess\" was NOT in the word."
     fi
+
+    masked=""
+
+    # Mask mystery word with underscores
+    for (( i=0; i<${#mystery}; i++ )); do
+        c=${mystery:i:1}
+
+        if [[ $guessed == *"$c"* ]]; then
+            masked+=$c
+        else
+            masked+="_"
+        fi
+    done
+
+    echo "$masked"
 done
